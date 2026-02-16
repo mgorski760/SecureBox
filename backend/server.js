cd82bf8b2f368edf93c683e01c66a476aa50d962
Marcin <mgorski760@gmail.com>
2026-02-15 21:55:25 -0600

    Added register api call. Need to troubleshoot why it isn't populating database.

--- backend/server.js
+++ backend/server.js
@@ -31,13 +31,19 @@ const client = new MongoClient(uri, {
   }
 });
 
+//database
+let db;
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    db = client.db("securebox_db");
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+    
   } finally {
     // Ensures that the client will close when you finish/error
     await client.close();
@@ -45,9 +51,35 @@ async function run() {
 }
 run().catch(console.dir);
 
-app.post('api/register', (req, res) => {
-    //expected json info
-    const {username, password} = req.body;
+app.post('api/register', async (req, res) => {
+    
+  try{
+    const {username, email, password} = req.body;
+
+
+    //Check if user already exists in the Database.
+    const existingUser = await db.collection('users').findOne({
+      $or: [{username}, {email}]
+    });
+
+    if(existingUser){
+      return res.status(400).json({ error: 'Username or email already exists' });
+    }
+
+    //Encrypt password
+    const hashedPassword = await bcrypt.hash(password, SALT_ROUNDS);
+
+    await db.collection('users').insertOne({
+      username,
+      email,
+      password: hashedPassword,
+      createdAt: new Date()
+    });
+
+    res.status(201).json({ message: 'User created successfully' });
+  } catch (error){
+    res.status(500).json({ error: 'Server error. Failed to add user.' });
+  }
 
     
 
